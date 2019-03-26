5c58462c1d8ce0b27d3705907119659cd609db29
xiuming <xiuming@mit.edu>
2019-03-26 15:11:11 -0400

    edits

--- config.py
+++ config.py
@@ -2,18 +2,19 @@ import logging
 import logging_colorer # noqa: F401 # pylint: disable=unused-import
 
 logging_warn = logging.WARN
+# so that won't need to import a package just for its constants
+
+users = ['xiuminglib', 'commandline', 'job-dispatcher']
 
 
 def create_logger(file_abspath, level=logging.INFO):
     logging.basicConfig(level=level)
     logger = logging.getLogger()
     folder_names = file_abspath.split('/')
-    for pkg_name in ['xiuminglib', 'commandline']:
-        if pkg_name in folder_names:
+    for user in users:
+        if user in folder_names:
             thisfile = '/'.join(
-                folder_names[
-                    folder_names.index(pkg_name):
-                ]
+                folder_names[folder_names.index(user):]
             )
             return logger, thisfile
-    raise ValueError("Unidentified package")
+    raise ValueError("Unexpected user")
