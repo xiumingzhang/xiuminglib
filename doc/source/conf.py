5e085990683a5a038c36e8c2d8dd0779d18194bc
xiuming <xiuming@mit.edu>
2019-03-26 16:51:33 -0400

    use abspath for it to build locally

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -21,6 +21,7 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath('../../'))
 
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
