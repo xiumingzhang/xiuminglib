6bacb24e4811bd738ea56bb510692b188d6b974c
xiuming <xiuming@mit.edu>
2019-06-12 18:14:16 -0700

    disable import __all__ in __init__

--- xiuminglib/__init__.py
+++ xiuminglib/__init__.py
@@ -3,4 +3,4 @@ from os.path import dirname, join, abspath
 
 from .config import get_all
 __all__ = get_all(dirname(__file__))
-from . import * # noqa: F401,F403 # pylint: disable=wildcard-import
+# from . import * # noqa: F401,F403 # pylint: disable=wildcard-import
--- xiuminglib/io/__init__.py
+++ xiuminglib/io/__init__.py
@@ -2,4 +2,4 @@ from os.path import dirname
 
 from ..config import get_all
 __all__ = get_all(dirname(__file__))
-from . import * # noqa: F403,F401 # pylint: disable=wildcard-import
+# from . import * # noqa: F403,F401 # pylint: disable=wildcard-import
