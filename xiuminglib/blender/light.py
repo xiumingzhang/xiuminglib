768113c757e6839781d75ac13c355e798de4b874
xiuming <xiuming@mit.edu>
2019-07-17 15:33:53 -0700

    minor

--- xiuminglib/blender/light.py
+++ xiuminglib/blender/light.py
@@ -1,8 +1,8 @@
-"""Add functions in this module usually provide no setter for the lamp's 3D rotation,
-because one usually implicitly sets the rotation by pointing the light to an object
-(and specifying an up vector), by using :func:`point_light_to`.
+"""Add functions in this module usually provide no setter for the lamp's 3D
+rotation, because one usually implicitly sets the rotation by pointing the
+light to an object (and specifying an up vector), by using
+:func:`point_light_to`.
 """
-
 
 from os.path import abspath, basename
 import numpy as np
@@ -28,8 +28,10 @@ def point_light_to(light, target):
 
     # Point it to target
     direction = target - light.location
-    # Find quaternion that rotates lamp facing direction '-Z' so that it aligns with 'direction'
-    # This rotation is not unique because the rotated lamp can still rotate about direction vector
+    # Find quaternion that rotates lamp facing direction '-Z',
+    # so that it aligns with 'direction'
+    # This rotation is not unique because the rotated lamp can still rotate
+    # about direction vector
     # Specifying 'Y' gives the rotation quaternion with lamp's 'Y' pointing up
     rot_quat = direction.to_track_quat('-Z', 'Y')
     light.rotation_euler = rot_quat.to_euler()
@@ -38,22 +40,27 @@ def point_light_to(light, target):
     logger.info("Lamp '%s' points to %s now", light.name, target)
 
 
-def add_light_sun(xyz=(0, 0, 0), rot_vec_rad=(0, 0, 0), name=None, energy=1, size=0.1):
+def add_light_sun(xyz=(0, 0, 0), rot_vec_rad=(0, 0, 0), name=None,
+                  energy=1, size=0.1):
     """Adds a sun lamp that emits parallel light rays.
 
     Args:
-        xyz (tuple(float), optional): Location only used to compute light ray direction.
-        rot_vec_rad (tuple(float), optional): Rotations in radians around x, y and z.
+        xyz (tuple(float), optional): Location only used to compute light ray
+            direction.
+        rot_vec_rad (tuple(float), optional): Rotations in radians around x,
+            y and z.
         name (str, optional): Light name.
         energy (float, optional): Light intensity.
-        size (float, optional): Light size for ray shadow tracing. Use larger for softer shadows.
+        size (float, optional): Light size for ray shadow tracing. Use larger
+            for softer shadows.
 
     Returns:
         bpy_types.Object: Light added.
     """
     logger_name = thisfile + '->add_light_sun()'
 
-    bpy.ops.object.lamp_add(type='SUN', location=xyz, rotation=rot_vec_rad)
+    bpy.ops.object.lamp_add(
+        type='SUN', location=xyz, rotation=rot_vec_rad)
     sun = bpy.context.active_object
 
     if name is not None:
@@ -64,7 +71,8 @@ def add_light_sun(xyz=(0, 0, 0), rot_vec_rad=(0, 0, 0), name=None, energy=1, siz
     # Strength
     engine = bpy.context.scene.render.engine
     if engine == 'CYCLES':
-        sun.data.node_tree.nodes['Emission'].inputs['Strength'].default_value = energy
+        sun.data.node_tree.nodes['Emission'].inputs[
+            'Strength'].default_value = energy
     else:
         raise NotImplementedError(engine)
 
@@ -74,12 +82,14 @@ def add_light_sun(xyz=(0, 0, 0), rot_vec_rad=(0, 0, 0), name=None, energy=1, siz
     return sun
 
 
-def add_light_area(xyz=(0, 0, 0), rot_vec_rad=(0, 0, 0), name=None, energy=100, size=0.1):
+def add_light_area(xyz=(0, 0, 0), rot_vec_rad=(0, 0, 0), name=None,
+                   energy=100, size=0.1):
     """Adds an area light that emits light rays the lambertian way.
 
     Args:
         xyz (tuple(float), optional): Location.
-        rot_vec_rad (tuple(float), optional): Rotations in radians around x, y and z.
+        rot_vec_rad (tuple(float), optional): Rotations in radians around x,
+            y and z.
         name (str, optional): Light name.
         energy (float, optional): Light intensity.
         size (float, optional): Light size for ray shadow tracing.
@@ -91,7 +101,9 @@ def add_light_area(xyz=(0, 0, 0), rot_vec_rad=(0, 0, 0), name=None, energy=100, 
     logger_name = thisfile + '->add_light_area()'
 
     if (np.abs(rot_vec_rad) > 2 * np.pi).any():
-        logger.warning("Some input value falls outside [-2pi, 2pi]. Sure inputs are in radians?")
+        logger.warning(
+            ("Some input value falls outside [-2pi, 2pi]. "
+             "Sure inputs are in radians?"))
 
     bpy.ops.object.lamp_add(type='AREA', location=xyz, rotation=rot_vec_rad)
     area = bpy.context.active_object
@@ -104,7 +116,8 @@ def add_light_area(xyz=(0, 0, 0), rot_vec_rad=(0, 0, 0), name=None, energy=100, 
     # Strength
     engine = bpy.context.scene.render.engine
     if engine == 'CYCLES':
-        area.data.node_tree.nodes['Emission'].inputs['Strength'].default_value = energy
+        area.data.node_tree.nodes['Emission'].inputs[
+            'Strength'].default_value = energy
     else:
         raise NotImplementedError(engine)
 
@@ -138,7 +151,8 @@ def add_light_point(xyz=(0, 0, 0), name=None, energy=100):
     # Strength
     engine = bpy.context.scene.render.engine
     if engine == 'CYCLES':
-        point.data.node_tree.nodes['Emission'].inputs['Strength'].default_value = energy
+        point.data.node_tree.nodes['Emission'].inputs[
+            'Strength'].default_value = energy
     else:
         raise NotImplementedError(engine)
 
@@ -156,7 +170,8 @@ def add_light_spot(xyz=(0, 0, 0), name=None, energy=100, shadow_soft_size=0.1,
         xyz (tuple(float), optional): Location.
         name (str, optional): Light name.
         energy (float, optional): Light intensity.
-        shadow_soft_size (float, optional): Light size for raytracing the shadow.
+        shadow_soft_size (float, optional): Light size for raytracing the
+            shadow.
         spot_size (float, optional): Angle, in radians, of the spotlight beam.
         spot_blend (float, optional): Softness of the spotlight edge.
 
@@ -174,7 +189,8 @@ def add_light_spot(xyz=(0, 0, 0), name=None, energy=100, shadow_soft_size=0.1,
     # Strength
     engine = bpy.context.scene.render.engine
     if engine == 'CYCLES':
-        spot.data.node_tree.nodes['Emission'].inputs['Strength'].default_value = energy
+        spot.data.node_tree.nodes['Emission'].inputs[
+            'Strength'].default_value = energy
     else:
         raise NotImplementedError(engine)
 
@@ -190,15 +206,19 @@ def add_light_spot(xyz=(0, 0, 0), name=None, energy=100, shadow_soft_size=0.1,
     return spot
 
 
-def add_light_env(env=(1, 1, 1, 1), strength=1, rot_vec_rad=(0, 0, 0), scale=(1, 1, 1)):
+def add_light_env(env=(1, 1, 1, 1), strength=1, rot_vec_rad=(0, 0, 0),
+                  scale=(1, 1, 1)):
     r"""Adds environment lighting.
 
     Args:
-        env (tuple(float) or str, optional): Environment map. If tuple, it's RGB or RGBA, each
-            element of which :math:`\in [0,1]`. Otherwise, it's the path to an image.
+        env (tuple(float) or str, optional): Environment map. If tuple,
+            it's RGB or RGBA, each element of which :math:`\in [0,1]`.
+            Otherwise, it's the path to an image.
         strength (float, optional): Light intensity.
-        rot_vec_rad (tuple(float), optional): Rotations in radians around x, y and z.
-        scale (tuple(float), optional): If all changed simultaneously, then no effects.
+        rot_vec_rad (tuple(float), optional): Rotations in radians around x,
+            y and z.
+        scale (tuple(float), optional): If all changed simultaneously,
+            then no effects.
     """
     logger_name = thisfile + '->add_light_env()'
 
@@ -220,14 +240,16 @@ def add_light_env(env=(1, 1, 1, 1), strength=1, rot_vec_rad=(0, 0, 0), scale=(1,
     links = node_tree.links
 
     bg_node = nodes.new('ShaderNodeBackground')
-    links.new(bg_node.outputs['Background'], nodes['World Output'].inputs['Surface'])
+    links.new(bg_node.outputs['Background'],
+              nodes['World Output'].inputs['Surface'])
 
     if isinstance(env, tuple):
         # Color
         bg_node.inputs['Color'].default_value = env
 
         logger.name = logger_name
-        logger.warning("Environment is pure color, so rotation and scale have no effect")
+        logger.warning(("Environment is pure color, "
+                        "so rotation and scale have no effect"))
     else:
         # Environment map
         texcoord_node = nodes.new('ShaderNodeTexCoord')
@@ -236,7 +258,8 @@ def add_light_env(env=(1, 1, 1, 1), strength=1, rot_vec_rad=(0, 0, 0), scale=(1,
         mapping_node = nodes.new('ShaderNodeMapping')
         mapping_node.rotation = rot_vec_rad
         mapping_node.scale = scale
-        links.new(texcoord_node.outputs['Generated'], mapping_node.inputs['Vector'])
+        links.new(texcoord_node.outputs['Generated'],
+                  mapping_node.inputs['Vector'])
         links.new(mapping_node.outputs['Vector'], env_node.inputs['Vector'])
         links.new(env_node.outputs['Color'], bg_node.inputs['Color'])
 
