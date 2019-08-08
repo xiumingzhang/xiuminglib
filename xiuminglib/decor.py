aecfd843577387cf6f3d34884692a1c4a3239a20
xiuming <xiuming@mit.edu>
2019-08-08 02:42:40 -0400

    unknow vision machine commit before merging

--- xiuminglib/decor.py
+++ xiuminglib/decor.py
@@ -18,7 +18,7 @@ def timeit(somefunc):
         results = somefunc(*arg, **kwargs)
         t = time() - t0
         logger.name = logger_name
-        logger.info("    ... and done in %.2f seconds", t)
+        logger.info("... and finished in %f seconds", t)
         return results
 
     return wrapper
