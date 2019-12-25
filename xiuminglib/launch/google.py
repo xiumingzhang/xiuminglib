6ca65fffefa3f78d25501d90e11f83d6ad3b3956
xiuming <xiuming6zhang@gmail.com>
2019-12-24 18:27:11 -0800

    adds n_tasks_per_job so that we don't wait for hours for thousands of jobs to be submitted

--- xiuminglib/launch/google.py
+++ xiuminglib/launch/google.py
@@ -1,8 +1,10 @@
-from os.path import join, abspath
+from os.path import join, abspath, commonprefix
 from getpass import getuser
 from time import time
 from random import choices
 from string import ascii_uppercase, digits
+from math import ceil
+from multiprocessing import Pool
 
 from tqdm import tqdm
 
@@ -66,12 +68,12 @@ class Launcher():
         # Blaze parameters
         if blaze_dict is not None:
             for k, v in blaze_dict.items():
-                bash_cmd += ' --%s=%s' % (k, v)
+                bash_cmd += ' --{flag}={val}'.format(flag=k, val=v)
         # Job parameters
         if param_dict is not None:
             bash_cmd += ' --'
             for k, v in param_dict.items():
-                bash_cmd += ' --%s=%s' % (k, v)
+                bash_cmd += ' --{flag}={val}'.format(flag=k, val=v)
         # To avoid IO permission issues
         bash_cmd += ' --gfs_user=%s' % self.borg_user
         if self.print_instead:
@@ -94,25 +96,59 @@ class Launcher():
             retcode, _, _ = call(bash_cmd)
             assert retcode == 0, "Build failed"
 
-    def submit_to_borg(self, job_ids, param_dicts):
+    @staticmethod
+    def _divide_jobs(job_ids, param_dicts, n_tasks_per_job):
+        n_jobs = ceil(len(job_ids) / n_tasks_per_job)
+        # Get shared
+        shared_params = dict(
+            set.intersection(*(set(d.items()) for d in param_dicts)))
+        shared_id = commonprefix(job_ids)
+        # Collect task-specific parameters for each job
+        job_names, job_specific_params, job_n_tasks = [], [], []
+        for job_i in range(n_jobs):
+            start = job_i * n_tasks_per_job
+            end = min(start + n_tasks_per_job, len(param_dicts))
+            task_specific_params = {}
+            for task_params in param_dicts[start:end]:
+                for k, v in task_params.items():
+                    if k not in shared_params: # not shared
+                        if k not in task_specific_params:
+                            task_specific_params[k] = []
+                        task_specific_params[k].append(v)
+            # Sanity check: different parameters say the same number of tasks
+            n_tasks = [len(v) for _, v in task_specific_params.items()]
+            assert len(set(n_tasks)) == 1, \
+                "Value lists for task parameters must be of the same length"
+            job_n_tasks.append(n_tasks[0])
+            # These tasks constitute one job
+            job_specific_params.append(task_specific_params)
+            job_names.append(shared_id + '_job%04d' % job_i)
+        return job_names, shared_params, job_specific_params, job_n_tasks
+
+    def submit_to_borg(self, job_ids, param_dicts, n_tasks_per_job=1):
         assert isinstance(job_ids, list) and isinstance(param_dicts, list), \
             "If submitting just one job, make both arguments single-item lists"
-        n_jobs = len(job_ids)
-        assert n_jobs == len(param_dicts)
-        # If just one job or no parallel workers or printing only
+        assert len(job_ids) == len(param_dicts), \
+            "Numbers of job IDs and parameter dictionaries must be equal"
+        # Divide tasks into jobs
+        job_names, shared_params, job_specific_params, job_n_tasks = \
+            self._divide_jobs(job_ids, param_dicts, n_tasks_per_job)
+        # Sequential submissions, if just one job, or no parallel workers,
+        # or printing only
+        n_jobs = len(job_names)
         if n_jobs == 1 or self.borg_submitters == 0 or self.print_instead:
-            for job_id, param_dict in tqdm(
-                    zip(job_ids, param_dicts), total=n_jobs,
-                    desc="Submitting jobs to Borg"):
-                self._borg_run((job_id, param_dict))
-        # Multiple jobs are submitted by a pool of workers
+            for job_name, job_specific, n_tasks in tqdm(
+                    zip(job_names, job_specific_params, job_n_tasks),
+                    desc="Submitting jobs to Borg", total=n_jobs):
+                self._borg_run((job_name, shared_params, job_specific, n_tasks))
+        # Parallel submissions
         else:
-            from multiprocessing import Pool
             pool = Pool(self.borg_submitters)
             list(tqdm(pool.imap_unordered(
                 self._borg_run,
-                [(i, x) for i, x in zip(job_ids, param_dicts)]
-            ), total=len(job_ids), desc="Submitting jobs to Borg"))
+                [(x, shared_params, y, z) for x, y, z in
+                 zip(job_names, job_specific_params, job_n_tasks)]
+            ), total=len(job_names), desc="Submitting jobs to Borg"))
             pool.close()
             pool.join()
 
@@ -120,100 +156,127 @@ class Launcher():
         """.borg generation assumed temporary MPM.
         """
         logger_name = thisfile + '->Launcher:_borg_run()'
-        job_id, param_dict = args
-        borg_f = self.__gen_borg_file(job_id, param_dict)
+        job_name, shared_params, task_params, n_tasks = args
+        borg_f = self.__gen_borg_file(
+            job_name, shared_params, task_params, n_tasks)
         # Submit
         action = 'reload'
         # NOTE: runlocal doesn't work for temporary MPM: b/74472376
-        bash_cmd = 'borgcfg %s %s --skip_confirmation --borguser %s' \
-            % (borg_f, action, self.borg_user)
+        bash_cmd = 'borgcfg {f} {a} --skip_confirmation --borguser {u}'.format(
+            f=borg_f, a=action, u=self.borg_user)
         if self.print_instead:
             logger.name = logger_name
             logger.info("To launch the job on Borg, run:\n\t%s\n", bash_cmd)
         else:
             call(bash_cmd)
 
-    def __gen_borg_file(self, job_id, param_dict):
-        borg_file_str = self.___format_borg_file_str(job_id, param_dict)
-        out_dir = join(const.Dir.tmp, '{t}_{s}'.format(
-            s=''.join(choices(ascii_uppercase + digits, k=16)),
-            t=time()))
+    def __gen_borg_file(self, job_name, shared_params, task_params, n_tasks):
+        borg_file_str = self.___format_borg_file_str(
+            job_name, shared_params, task_params, n_tasks)
+        out_dir = join(const.Dir.tmp, '{t}_{s}'.format(s=_random_str(16),
+                                                       t=time()))
         makedirs(out_dir)
-        borg_f = join(out_dir, '%s.borg' % job_id)
+        borg_f = join(out_dir, '%s.borg' % job_name)
         with open(borg_f, 'w') as h:
             h.write(borg_file_str)
         return borg_f
 
-    def ___format_borg_file_str(self, job_id, param_dict):
+    @staticmethod
+    def ____to_str(v):
+        if isinstance(v, str):
+            v = "'%s'" % v
+        elif isinstance(v, int):
+            v = "%d" % v
+        elif isinstance(v, float):
+            v = "%f" % v
+        elif isinstance(v, list):
+            v = "'" + ",".join(str(x) for x in v) + "'"
+        else:
+            raise TypeError(type(v))
+        return v
+
+    def ___format_borg_file_str(
+            self, job_name, shared_params, task_params, n_tasks):
         tab = ' ' * 4
-        file_str = '''job %s = {
+        file_str = '''job {job_name} = {{
     // What cell should we run in?
-    runtime = {
+    runtime = {{
         // 'oregon' // automatically picks a Borg cell with free capacity
-        cell = '%s',
-    }
+        cell = '{cell}',
+    }}
 
     // What packages are needed?
-    packages {
-        package bin = {
+    packages {{
+        package bin = {{
             // A blaze label pointing to a `genmpm(temporal=1)` rule. Borgcfg will
             // build a "temporal MPM" on the fly out of files in the blaze-genfiles
             // directory. See go/temporal-mpm for full documentation.
-            blaze_label = '%s',
-        }
-    }
+            blaze_label = '{label}',
+        }}
+    }}
 
     // What program are we going to run?
-    package_binary = 'bin/%s'
+    package_binary = 'bin/{binary}'
 
     // What command line parameters should we pass to this program?
-    args = {
-    ''' % (job_id, self.cell, self.label, self.pkg_bin)
-        for i, (k, v) in enumerate(param_dict.items()):
-            if isinstance(v, str):
-                v = "'%s'" % v
-            elif isinstance(v, int):
-                v = "%d" % v
-            elif isinstance(v, float):
-                v = "%f" % v
-            elif isinstance(v, list):
-                v = "'" + ",".join(str(x) for x in v) + "'"
-            else:
-                raise TypeError(type(v))
+    args = {{
+    '''.format(job_name=job_name, cell=self.cell,
+               label=self.label, binary=self.pkg_bin)
+        # Add shared parameters
+        for i, (k, v) in enumerate(shared_params.items()):
+            v = self.____to_str(v)
+            str_ = '{tab}{flag} = {val},\n'
             if i == 0:
-                file_str += "%s%s%s = %s,\n" % (tab, tab, k, v)
-            else:
-                file_str += "%s%s%s%s = %s,\n" % (tab, tab, tab, k, v)
-        file_str += '''%s%s}
+                str_ = '{tab}' + str_
+            file_str += str_.format(tab=tab, flag=k, val=v)
+        # Add placeholders for task-specific parameters
+        for k, _ in task_params.items():
+            v = "'%{}%'".format(k)
+            file_str += '{tab}{tab}{flag} = {val},\n'.format(
+                tab=tab, flag=k, val=v)
+        file_str += '{tab}}}'.format(tab=tab)
+        # Fill in task-specific parameters
+        file_str += '''
+
+    replicas = {replicas}
+
+    task_args {{'''.format(replicas=n_tasks)
+        for k, vlist in task_params.items():
+            file_str += '\n{tab}{tab}{flag} = {val_list}'.format(
+                tab=tab, flag=k, val_list=vlist)
+        # The rest
+        file_str += '''
+    }}
 
     // What resources does this program need to run?
-    requirements = {
+    requirements = {{
         autopilot = true,
-        autopilot_params {
+        autopilot_params {{
             // Let autopilot increase limits past the Borg pickiness limit
-            scheduling_strategy = "NO_SCHEDULING_SLO",
-        }
+            scheduling_strategy = 'NO_SCHEDULING_SLO',
+        }}
         // ram = 1024M,
         // use_ram_soft_limit = true,
-        local_ram_fs_dir { d1 = { size = %s } },
+        local_ram_fs_dir {{ d1 = {{ size = {local_ram_fs_dir_size} }} }},
         // cpu = 12,
-    }
+    }}
 
     // How latency-sensitive is this program?
-    appclass = {
+    appclass = {{
         type = 'LATENCY_TOLERANT_SECONDARY',
-    }
+    }}
 
-    permissions = {
-        user = '%s',
-    }
+    permissions = {{
+        user = '{user}',
+    }}
 
-    scheduling = {
-        priority = %d,
-    ''' % (tab, tab, self.local_ram_fs_dir_size, self.borg_user, self.priority)
+    scheduling = {{
+        priority = {priority},
+    '''.format(local_ram_fs_dir_size=self.local_ram_fs_dir_size,
+               user=self.borg_user, priority=self.priority)
         if self.priority == 0:
-            file_str += '''%s}
-    }''' % tab
+            file_str += '''{tab}}}
+    }}'''.format(tab=tab)
         else: # e.g., P115 needs a batch strategy
             file_str += '''
         batch_quota {
@@ -222,3 +285,7 @@ class Launcher():
     }
 }'''
         return file_str
+
+
+def _random_str(l):
+    return ''.join(choices(ascii_uppercase + digits, k=l))
