7d090d430c91fac3d4bb9b43598f814b78a13d7b
xiuming <xiuming@mit.edu>
2021-07-11 22:52:34 -0400

    revamp vis.plot.Plot to avoid repeating x, y, z axes

--- xiuminglib/vis/plot.py
+++ xiuminglib/vis/plot.py
@@ -13,51 +13,44 @@ from ..os import makedirs, open_file
 class Plot:
     def __init__(
             self,
-            legend_fontsize=20,
-            legend_loc=0,
-            figsize=(14, 14),
-            figtitle=None,
-            figtitle_fontsize=20,
-            xlabel=None,
-            xlabel_fontsize=20,
-            ylabel=None,
-            ylabel_fontsize=20,
-            zlabel=None,
-            zlabel_fontsize=20,
-            xlim=None,
-            ylim=None,
-            zlim=None,
-            xticks=None,
-            xticks_fontsize=10,
-            xticks_rotation=0,
-            yticks=None,
-            yticks_fontsize=10,
-            yticks_rotation=0,
-            zticks=None,
-            zticks_fontsize=10,
-            zticks_rotation=0,
+            figsize=None,
+            legend_labels=None, legend_fontsize=None, legend_loc='best',
+            figtitle=None, figtitle_fontsize=None,
+            axis_labels=None, axis_label_fontsizes=None,
+            axis_lim=None,
+            axis_ticks=None, axis_tick_labels=None,
+            axis_tick_label_fontsizes=None, axis_tick_label_rotations=None,
             grid=True,
-            labels=None,
             outpath=None):
         """Plotter.
 
         Args:
+            figsize (tuple, optional): Width and height of the figure in inches.
+            legend_labels (list, optional): Legend labels.
             legend_fontsize (int, optional): Legend font size.
             legend_loc (str, optional): Legend location: ``'best'``,
                 ``'upper right'``, ``'lower left'``, ``'right'``,
                 ``'center left'``, ``'lower center'``, ``'upper center'``,
-                ``'center'``, etc. Effective only when ``labels`` is not
+                ``'center'``, etc. Effective only when ``legend_labels`` is not
                 ``None``.
-            figsize (tuple, optional): Width and height of the figure in inches.
             figtitle (str, optional): Figure title.
-            *_fontsize (int, optional): Font size.
-            ?label (str, optional): Axis labels.
-            ?lim (array_like, optional): Axis min. and max. ``None`` means auto.
-            ?ticks (array_like, optional): Axis tick values. ``None`` means
-                auto.
-            ?ticks_rotation (float, optional): Tick rotation in degrees.
+            figtitle_fontsize (int, optional): Font size.
+            axis_labels (dict, optional): Axis labels with ``'x'``, ``'y'``,
+                and/or ``'z'`` as keys.
+            axis_label_fontsizes (dict, optional): Axis label font sizes with
+                ``'x'``, ``'y'``, and/or ``'z'`` as keys.
+            axis_lim (dict, optional): Mapping ``'x'``, ``'y'``, or ``'z'`` to
+                an ``array_like`` of axis min. and max.
+            axis_ticks (dict, optional): Axis tick locations, mapping ``'x'``,
+                ``'y'``, or ``'z'`` to an ``array_like`` of floats.
+            axis_tick_labels (dict, optional): Axis tick labels, mapping
+                ``'x'``, ``'y'``, or ``'z'`` to a list of strings.
+            axis_tick_label_fontsizes (dict, optional): Axis tick label font
+                sizes, mapping ``'x'``, ``'y'``, or ``'z'`` to a float.
+            axis_tick_label_rotations (dict, optional): Axis tick label
+                rotations in degrees, mapping ``'x'``, ``'y'``, or ``'z'``
+                to a float.
             grid (bool, optional): Whether to draw grid.
-            labels (list, optional): Labels.
             outpath (str, optional): Path to which the plot is saved to. Should
                 end with ``'.png'``, and ``None`` means to ``const.Dir.tmp``.
         """
@@ -66,32 +59,39 @@ class Plot:
         import matplotlib.pyplot as plt
         #
         self.plt = plt
+        self.legend_labels = legend_labels
         self.legend_fontsize = legend_fontsize
         self.legend_loc = legend_loc
         self.figsize = figsize
         self.figtitle = figtitle
         self.figtitle_fontsize = figtitle_fontsize
-        self.xlabel = xlabel
-        self.xlabel_fontsize = xlabel_fontsize
-        self.ylabel = ylabel
-        self.ylabel_fontsize = ylabel_fontsize
-        self.zlabel = zlabel
-        self.zlabel_fontsize = zlabel_fontsize
-        self.xlim = xlim
-        self.ylim = ylim
-        self.zlim = zlim
-        self.xticks = xticks
-        self.xticks_rotation = xticks_rotation
-        self.xticks_fontsize = xticks_fontsize
-        self.yticks = yticks
-        self.yticks_rotation = yticks_rotation
-        self.yticks_fontsize = yticks_fontsize
-        self.zticks = zticks
-        self.zticks_rotation = zticks_rotation
-        self.zticks_fontsize = zticks_fontsize
+        self.axis_labels = self._init_axis_dict(axis_labels)
+        self.axis_label_fontsizes = self._init_axis_dict(axis_label_fontsizes)
+        self.axis_lim = self._init_axis_dict(axis_lim)
+        self.axis_ticks = self._init_axis_dict(axis_ticks)
+        self.axis_tick_labels = self._init_axis_dict(axis_tick_labels)
+        self.axis_tick_label_fontsizes = self._init_axis_dict(
+            axis_tick_label_fontsizes)
+        self.axis_tick_label_rotations = self._init_axis_dict(
+            axis_tick_label_rotations)
         self.grid = grid
-        self.labels = labels
         self.outpath = outpath
+
+    @staticmethod
+    def _init_axis_dict(overrides):
+        default = {'x': None, 'y': None, 'z': None}
+        if overrides is None:
+            return default
+        for k, v in overrides.items():
+            default[k] = v
+        return default
+
+    def _create_fig(self):
+        if self.figsize is None:
+            fig = self.plt.figure()
+        else:
+            fig = self.plt.figure(figsize=self.figsize)
+        return fig
 
     def _savefig(self, outpath, contents_only=False, dpi=None):
         # Make directory, if necessary
@@ -109,47 +109,57 @@ class Plot:
                 self.plt.savefig(h, bbox_inches='tight', dpi=dpi)
 
     def _add_legend(self, plot_objs):
-        if self.labels is None:
+        if self.legend_labels is None:
             return
         n_plot_objs = len(plot_objs)
-        assert (len(self.labels) == n_plot_objs), (
-            "Number of labels must equal number of plot objects; "
-            "use None for object without a label")
+        assert (len(self.legend_labels) == n_plot_objs), (
+            "Number of legend labels must equal number of plot objects; "
+            "use None for object without a legend label")
         for i in range(n_plot_objs):
-            plot_objs[i].set_label(self.labels[i])
-        self.plt.legend(fontsize=self.legend_fontsize, loc=self.legend_loc)
+            plot_objs[i].set_label(self.legend_labels[i])
+        if self.legend_fontsize is None:
+            self.plt.legend(loc=self.legend_loc)
+        else:
+            self.plt.legend(fontsize=self.legend_fontsize, loc=self.legend_loc)
 
     def _add_axis_labels(self, ax):
-        if self.xlabel is not None:
-            ax.set_xlabel(self.xlabel, fontsize=self.xlabel_fontsize)
-        if self.ylabel is not None:
-            ax.set_ylabel(self.ylabel, fontsize=self.ylabel_fontsize)
-        if self.zlabel is not None:
-            ax.set_zlabel(self.zlabel, fontsize=self.zlabel_fontsize)
+        for axis, label in self.axis_labels.items():
+            if label is None:
+                continue
+            fontsize = self.axis_label_fontsizes[axis]
+            set_func = getattr(ax, f'set_{axis}label')
+            set_func(label, fontsize=fontsize)
 
     def _set_axis_ticks(self, ax):
-        # FIXME: If xticks is not provided, xticks_fontsize and xticks_rotation have
-        # no effect, which shouldn't be the case
-        if self.xticks is not None:
-            ax.set_xticklabels(
-                self.xticks, fontsize=self.xticks_fontsize,
-                rotation=self.xticks_rotation)
-        if self.yticks is not None:
-            ax.set_yticklabels(
-                self.yticks, fontsize=self.yticks_fontsize,
-                rotation=self.yticks_rotation)
-        if self.zticks is not None:
-            ax.set_zticklabels(
-                self.zticks, fontsize=self.zticks_fontsize,
-                rotation=self.zticks_rotation)
+        # Tick locations
+        for axis, ticks in self.axis_ticks.items():
+            if ticks is None:
+                continue
+            set_func = getattr(ax, f'set_{axis}ticks')
+            set_func(ticks)
+        # Tick labels
+        for axis, tick_labels in self.axis_tick_labels.items():
+            if tick_labels is None:
+                continue
+            set_func = getattr(ax, f'set_{axis}ticklabels')
+            set_func(tick_labels)
+        # Tick label font size
+        for axis, tick_label_fontsize in self.axis_tick_label_fontsizes.items():
+            if tick_label_fontsize is None:
+                continue
+            ax.tick_params(axis=axis, labelsize=tick_label_fontsize)
+        # Tick rotation
+        for axis, tick_label_rotation in self.axis_tick_label_rotations.items():
+            if tick_label_rotation is None:
+                continue
+            ax.tick_params(axis=axis, labelrotation=tick_label_rotation)
 
     def _set_axis_lim(self, ax):
-        if self.xlim is not None:
-            ax.set_xlim(*self.xlim)
-        if self.ylim is not None:
-            ax.set_ylim(*self.ylim)
-        if self.zlim is not None:
-            ax.set_zlim(*self.zlim)
+        for axis, lim in self.axis_lim.items():
+            if lim is None:
+                continue
+            set_func = getattr(ax, f'set_{axis}lim')
+            set_func(*lim)
 
     @staticmethod
     def _set_axes_equal(ax, xyz):
@@ -171,7 +181,10 @@ class Plot:
 
     def _set_title(self, ax):
         if self.figtitle is not None:
-            ax.set_title(self.figtitle, fontsize=self.figtitle_fontsize)
+            if self.figtitle_fontsize is None:
+                ax.set_title(self.figtitle)
+            else:
+                ax.set_title(self.figtitle, fontsize=self.figtitle_fontsize)
 
     def bar(self, y, group_width=0.8):
         """Bar plot.
@@ -187,7 +200,7 @@ class Plot:
         """
         outpath = join(const.Dir.tmp, 'bar.png') if self.outpath is None \
             else self.outpath
-        fig = self.plt.figure(figsize=self.figsize)
+        fig = self._create_fig()
         ax = fig.add_subplot(111)
         self._set_title(ax)
         # Ensure y is 2D, with columns representing values within groups
@@ -238,7 +251,7 @@ class Plot:
         #
         outpath = join(const.Dir.tmp, 'scatter3d.png') if self.outpath is None \
             else self.outpath
-        fig = self.plt.figure(figsize=self.figsize)
+        fig = self._create_fig()
         ax = fig.add_subplot(111, projection='3d')
         self._set_title(ax)
         # Prepare kwargs to scatter()
@@ -299,7 +312,7 @@ class Plot:
         """
         outpath = join(const.Dir.tmp, 'line.png') if self.outpath is None \
             else self.outpath
-        fig = self.plt.figure(figsize=self.figsize)
+        fig = self._create_fig()
         ax = fig.add_subplot(111)
         self._set_title(ax)
         # Prepare kwargs to scatter()
